36f33c41490bcbdbc3a5ffaa7b9cce5fb462d022
Vaal34 <5619@holbertonstudents.com>
2024-06-27 15:51:19 +0200

    Refactor Dart code to simplify double printing

--- 0x00-hello-dart/3-print_double.dart
+++ 0x00-hello-dart/3-print_double.dart
@@ -1,5 +1,4 @@
 void main() {
-    var n = 3.14159265359;
-    n = double.parse(n.toStringAsFixed(2));
-    print('Double: ${n}\n');
+  double n = 3.14159265359;
+  print('Double: ${n.toStringAsFixed(2)}\n');
 }
