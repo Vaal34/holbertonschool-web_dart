85c5453ae57ca80cd02efe0e52914c6b12e2185e
Vaal34 <5619@holbertonstudents.com>
2024-07-04 13:57:45 +0200

    Add outer_inner function to print a personalized greeting in Dart

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,7 +1,7 @@
 void outer(String name, String id){
     String inner(){
         var names = name.split(' ');
-        return "Hello Agent ${names[1][0]}.${names[0]}! Your ID is $id.";
+        return "Hello Agent ${names[1][0]}.${names[0]}! Your id is $id";
     }
     print(inner());
 }
